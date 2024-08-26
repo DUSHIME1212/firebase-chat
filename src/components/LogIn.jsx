d36b5e29a77df77cd251c826f5315b10f05fa685
DUSHIME1212 <mdonavan33@gmail.com>
2024-08-26 12:46:59 +0200

    "Updated LogIn component with new UI and added avatar upload functionality"

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LogIn = () => {
-  return (
-    <div>LogIn</div>
-  )
-}
+  const [avatar, Setavatar] = useState({
+    file: null,
+    url: "",
+  });
 
-export default LogIn
+  function handleAvatar(e) {
+    Setavatar({
+      file: e.target.files[0],
+      url: URL.createObjectURL(e.target.files[0]),
+    });
+  }
+  return (
+    <div className="w-full flex">
+      {/* left */}
+      <div className="w-1/2 flex flex-col gap-4 p-16 justify-center text-center h-full">
+        <h2 className="text-5xl">Welcome to our app</h2>
+        <form action="" className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="p-2 rounded-xl"
+            name="email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="p-2 rounded-xl"
+            name="password"
+          />
+          <button type="submit" className="w-full p-2 bg-blue-500 rounded-xl">
+            Sign in
+          </button>
+        </form>
+      </div>
+      {/* left */}
+      <div className="w-1/2 flex flex-col gap-4 p-16 justify-center text-center h-full">
+        <h2 className="text-3xl">Create an account to our app</h2>
+        <form action="" className="flex flex-col gap-4">
+          <label htmlFor="file" className="gap-4 flex flex-col">
+            <h2 className="">Upload an image</h2>
+            <img
+              src={avatar.url || "./avatar.png"}
+              className="size-16 object-cover rounded-xl"
+              alt=""
+            />
+          </label>
+
+          <input
+            type="file"
+            placeholder="File"
+            className="hidden"
+            onChange={handleAvatar}
+            id="file"
+            name="file"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="p-2 rounded-xl"
+            name="email"
+          />
+          <input
+            type="text"
+            placeholder="UserName"
+            className="p-2 rounded-xl"
+            name="Username"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="p-2 rounded-xl"
+            name="password"
+          />
+          <button type="submit" className="w-full p-2 bg-blue-500 rounded-xl">
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
